27470a3901007e951991343bab575f56b0275baa
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-10-14 12:40:02 +0200

    Normalize dataset values and features in load_dataset function

--- alt_batch/train.py
+++ alt_batch/train.py
@@ -98,8 +98,6 @@ class BipartiteAltBatcher(nn.Module):
 def load_dataset(args):
     dataset = data.load_dataset(args.dataset, get_edges=True)
 
-    print("dataset: ", dataset)
-
     u_to_idx, v_to_idx, us_to_edges, vs_to_edges = data.get_edge_lists(dataset)
 
     u_labels = torch.zeros(len(us_to_edges), dtype=torch.long)
