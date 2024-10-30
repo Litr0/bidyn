ccc59d5a7daf6b53ffc35bf849fe5e6bf4987141
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-10-30 14:54:37 +0100

    Uncomment debug print statements in train function for u_label_counts and unique items

--- alt_batch/train.py
+++ alt_batch/train.py
@@ -657,22 +657,22 @@ def train(args, dataset):
         # analyze embs
         if args.analyze and (task == "link" or is_best):
             from sklearn.manifold import TSNE
-            u_label_counts = Counter(u_labels.numpy())
+            """ u_label_counts = Counter(u_labels.numpy())
             print("u_label_counts:", u_label_counts)
             print("len v_to_edges:", len(vs_to_edges))   
             print("len v_to_idx:", len(v_to_idx))
-            print("v-to-idx:", v_to_idx) 
+            print("v-to-idx:", v_to_idx)  """
             embs = torch.cat((u_embs[:-1], v_embs[:-1])).detach().cpu().numpy()
             embs_2d = TSNE().fit_transform(embs)
             xs, ys = zip(*embs_2d)
             xs, ys = list(xs), list(ys)
-            """ colors = ["red" if l == 1 else "blue" for l in u_labels]
+            colors = ["red" if l == 1 else "blue" for l in u_labels]
             colors += ["green"]*(len(v_embs)-1)
             plt.scatter(xs, ys, color=colors, alpha=0.3)
             xrs = [x for x, l in zip(xs, u_labels) if l == 1]
             yrs = [y for y, l in zip(ys, u_labels) if l == 1]
-            plt.scatter(xrs, yrs, color="red", alpha=0.9) """
-            colors = []
+            plt.scatter(xrs, yrs, color="red", alpha=0.9)
+            """ colors = []
             items = []
             for i, label in enumerate(u_labels):
                 if label == 1:
@@ -687,7 +687,7 @@ def train(args, dataset):
             print("Color counts:", color_counts)
             print("Number of unique colors:", len(set(colors)))
             print("Items:", items)
-            print("unique items:", len(set(items)))  
+            print("unique items:", len(set(items)))   """
             plt.scatter(xs, ys, color=colors, alpha=0.3)
             xrs = [x for x, l in zip(xs, u_labels) if l == 1]
             yrs = [y for y, l in zip(ys, u_labels) if l == 1]
