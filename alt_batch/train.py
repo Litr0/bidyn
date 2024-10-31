4bbda5293a248d29dd3827479b273ddc3a0c5478
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-10-31 12:23:42 +0100

    Add print statement to display labels in load_dataset function

--- alt_batch/train.py
+++ alt_batch/train.py
@@ -109,6 +109,7 @@ def load_dataset(args):
         if dataset["labels"][u] == 1:
             n_pos_labels += 1
     for v, i in v_to_idx.items():
+        print(dataset["labels"][v])
         if dataset["labels"][v] == 1:
             if dataset["labels_items"][v][1] not in bad_items:
                 bad_items.append(dataset["labels_items"][u][1])
