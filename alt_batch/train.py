Fix mean feature calculation for edge labels by correcting variable order in dataset loading

alt_batch/train.py
@@ -102,8 +102,8 @@ def load_dataset(args):
 
     print("First 5 of us_to_edges_labels:", us_to_edges_labels[:5])
 
-    feats_mean_us_to_edges_label_one = [np.mean([f for u, v, t, f, label in l if label == 1], axis=0) for l in us_to_edges_labels]
-    feats_mean_us_to_edges_label_zero = [np.mean([f for u, v, t, f, label in l if label == 0], axis=0) for l in us_to_edges_labels]
+    feats_mean_us_to_edges_label_one = [np.mean([f for t, v, f, label in l if label == 1], axis=0) for l in us_to_edges_labels]
+    feats_mean_us_to_edges_label_zero = [np.mean([f for t, v, f, label in l if label == 0], axis=0) for l in us_to_edges_labels]
 
     print("Mean feats for label 1:")
     print(np.mean(feats_mean_us_to_edges_label_one, axis=0))