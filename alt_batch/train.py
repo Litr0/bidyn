Fix mean feature calculation for edge labels by including label in feature extraction

alt_batch/train.py
@@ -100,8 +100,8 @@ def load_dataset(args):
 
     u_to_idx, v_to_idx, us_to_edges, vs_to_edges , us_to_edges_labels, vs_to_edges_labels= data.get_edge_lists(dataset)
 
-    feats_mean_us_to_edges_label_one = [np.mean([f for u, v, t, f in l if label == 1], axis=0) for l in us_to_edges_labels]
-    feats_mean_us_to_edges_label_zero = [np.mean([f for u, v, t, f in l if label == 0], axis=0) for l in us_to_edges_labels]
+    feats_mean_us_to_edges_label_one = [np.mean([f for u, v, t, f, label in l if label == 1], axis=0) for l in us_to_edges_labels]
+    feats_mean_us_to_edges_label_zero = [np.mean([f for u, v, t, f, label in l if label == 0], axis=0) for l in us_to_edges_labels]
 
     print("Mean feats for label 1:")
     print(np.mean(feats_mean_us_to_edges_label_one, axis=0))