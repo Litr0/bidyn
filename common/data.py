bce6527626d7a4f2b2309f7aba5cce8ef796bf95
Litr0 <gonzalo.sobarzoa@gmail.com>
2025-02-06 15:17:13 +0100

    Update edge list processing to use label_users correctly

--- common/data.py
+++ common/data.py
@@ -162,11 +162,11 @@ def get_edge_lists(dataset):
     error_1 = 0
     success_0 = 0
     error_0 = 0
-    for (u, v, t), feats, label, us in tqdm(zip(dataset["edges"], edge_feats, dataset["labels_users"]), total=len(dataset["edges"])):
+    for (u, v, t), feats, label_us in tqdm(zip(dataset["edges"], edge_feats, dataset["labels_users"]), total=len(dataset["edges"])):
         if not (dataset["asin_filter"][u] and dataset["buyer_filter"][v]):
             continue
-        us_to_edges_labels[u_to_idx[u]].append((t, v_to_idx[v], feats, label))
-        vs_to_edges_labels[v_to_idx[v]].append((t, u_to_idx[u], feats, label))
+        us_to_edges_labels[u_to_idx[u]].append((t, v_to_idx[v], feats, label_us[0]))
+        vs_to_edges_labels[v_to_idx[v]].append((t, u_to_idx[u], feats, label_us[0]))
 
     # including more neighbors gives diminishing returns, so still subsample
     if dataset["name"] in ["reddit", "steam_2017_new_swapped"]:
